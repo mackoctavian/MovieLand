4bf4575ac96ba0434da9d4fbd0bf1b0d0585a8c9
Mack Octavian <maestrooctavian@gmail.com>
2024-02-17 12:34:18 +0300

    Added a searchMovies function to fetch movies from external API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+
+const API_URL = 'http://www.omdbapi.com?apikey=b8799a99'
 
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        console.log(data.Search)
+    } 
+    useEffect(() => {
+        searchMovies('Spiderman');
+    },[])
     return(
         <div>
             <h1>Hello React</h1>
@@ -8,4 +19,4 @@ const App = () => {
     )
 }
 
-export default App;
+export default App; 
